fe4e6b3dc1d5cb0443634c065281b1891eb43885
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2024-09-09 19:56:19 -0400

    Update TwoSum.java
    Solution is now O(n)

--- TwoSum.java
+++ TwoSum.java
@@ -4,17 +4,16 @@ public class TwoSum {
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] solution = {-1, -1};
-        outerloop:
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        int[] solution = new int[2];
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    solution[0] = i;
-                    solution[1] = j;
-                    break outerloop;
-                }
+            int diff = target - nums[i];
+            if(hashMap.containsKey(diff)) {
+                return new int[] {hashMap.get(diff), i};
+            } else {
+                hashMap.put(nums[i], i);
             }
         }
-        return solution;
+        return new int[] {};
     }
 }
