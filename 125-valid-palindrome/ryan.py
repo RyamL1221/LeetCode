fd0693516074db0f8561d99e1ef369bc2945d807
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-08 15:51:09 -0500

    Solved #125

---
+++ 125-valid-palindrome/ryan.py
@@ -0,0 +1,17 @@
+# Runtime 9ms (89.54%), Memory: 14.14MB (29.91%)
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        left, right = 0, len(s) - 1
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
