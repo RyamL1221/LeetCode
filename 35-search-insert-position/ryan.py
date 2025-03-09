6270e178c8e568b45b3c8538e3b1dc1a0f834442
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-08 23:23:09 -0500

    Solved #35

---
+++ 35-search-insert-position/ryan.py
@@ -0,0 +1,21 @@
+# Runtime: 0ms (100.00%), Memory: 13.05MB (23.38%)
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (right + left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
