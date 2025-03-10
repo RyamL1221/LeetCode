c82cb1aef3ce73fd0dbdf2d2b737d76c67104836
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-09 20:50:07 -0400

    Solved #215

---
+++ 215-kth-largest-element-in-array/ryan.py
@@ -0,0 +1,16 @@
+# Runtime: 686ms (9.85%), Memory: 19.50MB (90.21%)
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        min_heap = []
+
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        return min_heap[0]
