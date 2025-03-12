61f174ea20b1e39a958ee1394d0dd3c4fff6c95f
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-11 21:09:17 -0400

    Solved #167

---
+++ 167-two-sum-II-input-array-is-sorted/ryan.py
@@ -0,0 +1,19 @@
+# Runtime: 8ms (29.49%), Memory: 12.89MB (69.71%)
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum < target:
+                left += 1
+            elif current_sum > target:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
+        return -1
+        
