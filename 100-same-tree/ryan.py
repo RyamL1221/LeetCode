5e54caba654ba944f70076fc6150d35a4cfd09a3
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-13 16:41:22 -0400

    Solved #100

---
+++ 100-same-tree/ryan.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Runtime: 0ms (100.00%), Memory: 12.40MB (99.05%)
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        
