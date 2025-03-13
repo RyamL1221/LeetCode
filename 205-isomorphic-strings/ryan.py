373a87cd6313e0214749331dc0e8a70e78207e8c
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-03-13 16:33:03 -0400

    Solved #205

---
+++ 205-isomorphic-strings/ryan.py
@@ -0,0 +1,25 @@
+# Runtime: 10ms (61.27%), Memory: 15.31MB (8.98%)
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s,t):
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
+        return True 
