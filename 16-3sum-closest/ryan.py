3f2c2442b7366874ebd7b1b93909c72e23162675
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-06-15 19:45:03 -0400

    Solved #16

---
+++ 16-3sum-closest/ryan.py
@@ -0,0 +1,41 @@
+# Runtime: 384 ms (90.84), Memory: 12.43 MB (67.65%)
+"""
+    Variation of Three-Sum
+    Solution:
+        Fix one element in the array, and run two pointer on the rest of the array and keep track of how close these sums are to the target
+"""
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        min_difference = float('inf')
+        result_sum = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+
+                if current_sum == target:
+                    return target
+                elif current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+                
+                current_difference = abs(target - current_sum)
+
+                if current_difference < min_difference:
+                    result_sum = current_sum
+                    min_difference = current_difference
+        
+        return result_sum
+
+
+        
