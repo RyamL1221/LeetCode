f439f61909d6f868116b21eac0d8b650ed8dd04e
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-08-23 10:53:44 -0400

    Solved #300

---
+++ 300-longest-increasing-subsequence/ryan.py
@@ -0,0 +1,11 @@
+# Runtime: 1803ms (28.01%); Memory: 18.26MB (19.26%)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+        
+        return max(dp)
