724b0b1456a305c10a8301646bda376d680e763d
Ryan Lum <117312164+RyamL1221@users.noreply.github.com>
2025-08-25 20:27:53 -0400

    Solved #55

---
+++ 55-jump-game/ryan.py
@@ -0,0 +1,10 @@
+# Runtime: O(n), 9ms (93.69%); Memory: O(1), 18.80MB (26.13%)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
